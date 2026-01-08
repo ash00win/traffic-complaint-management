da5f9889944d2494673e38b55813238b3d49f72d
Ashwin <ashwinjoykuttikkadan@gmail.com>
2026-01-09 00:54:03 +0530

    Fix Django settings module for Render build

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+export DJANGO_SETTINGS_MODULE=backend.backend.settings
+export PYTHONPATH=/opt/render/project/src
+
 pip install -r requirements.txt
 python backend/manage.py collectstatic --noinput
 python backend/manage.py migrate
