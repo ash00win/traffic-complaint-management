26edead64c95ceb1f8e46d45b477d4bc93cddc39
Ashwin <ashwinjoykuttikkadan@gmail.com>
2026-01-09 00:28:21 +0530

    Prepare backend for Render deployment

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -28,7 +28,9 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', cast=bool)
 
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(',')
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='*').split(',')
+
+
 
 
 
